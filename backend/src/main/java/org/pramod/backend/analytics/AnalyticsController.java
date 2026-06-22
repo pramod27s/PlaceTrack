e23b8da0ece924f335d675955af953c1c58b3439
pramod27s <mrpramod3@gmail.com>
2026-06-22 22:29:50 +0530

    nil

--- backend/src/main/java/org/pramod/backend/analytics/AnalyticsController.java
+++ backend/src/main/java/org/pramod/backend/analytics/AnalyticsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AnalyticsController {
 
+
     private final AnalyticsService analyticsService;
 
     @GetMapping("/overview")
